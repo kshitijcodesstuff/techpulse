41f2c7dcd27d490bf1b39b8031644b38607f3d73
kshitijsgithub <121218323+kshitijsgithub@users.noreply.github.com>
2024-03-01 08:07:19 +0530

    attempt

--- app/create-post/page.tsx
+++ app/create-post/page.tsx
@@ -1,6 +1,6 @@
 import CreatePostForm from "@/components/CreatePostForm";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/configs/auth/authOptions";
 import { redirect } from "next/navigation";
 export default async function CreatePost(){
     const session = await getServerSession(authOptions);
